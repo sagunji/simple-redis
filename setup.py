7a04e2f6acb71702e1c692ad370d7022325582a5
sagunji <karanjit.sagun01@gmail.com>
2018-11-01 11:15:38 +0545

    master: Simple redis library

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="SimpleRedis",
+    version="0.1",
+    install_requires=requirements,
+    packages=["simpleredis"],
+    license="Creative Commons Attribution-Noncommercial-Share Alike license",
+    long_description=open("README.md").read(),
+)
