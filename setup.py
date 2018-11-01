e578d88433fe172f91498ed2ff7c3944daf5606b
sagunji <karanjit.sagun01@gmail.com>
2018-11-01 15:25:25 +0545

    master: Move redis code to main folder

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name="SimpleRedis",
     version="0.1",
     install_requires=requirements,
-    packages=["simpleredis"],
+    packages=["simple-redis"],
     license="Creative Commons Attribution-Noncommercial-Share Alike license",
     long_description=open("README.md").read(),
 )
